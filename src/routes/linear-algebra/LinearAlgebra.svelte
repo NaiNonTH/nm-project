df672cb54a3a33c48d7ad7b316f61489cdb964f9
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-09-14 00:11:36 +0700

    Simplify the code + add heading

---
+++ src/routes/linear-algebra/LinearAlgebra.svelte
@@ -0,0 +1,31 @@
+<script>
+	import MatrixInput from '$lib/components/MatrixInput.svelte';
+	import Answer from '$lib/components/Answer.svelte';
+
+    export let func;
+    export let joinMatrix = false;
+
+	import { joinedMatrix, matrixIsInvalid, linearAlgebra } from '$lib/utils/misc.js';
+
+	let A, B, size;
+	let result = null;
+
+	$: AisInvalid = matrixIsInvalid(A, size, size);
+	$: BisInvalid = matrixIsInvalid(B, size, 1);
+
+    function submit() {
+        if (joinMatrix)
+            result = linearAlgebra(func, joinedMatrix(A, B));
+        else
+            result = linearAlgebra(func, A, B);
+    }
+</script>
+
+<form on:submit|preventDefault={submit}>
+	<MatrixInput bind:size bind:A bind:B />
+	<div class="button-zone">
+		<button disabled={AisInvalid || BisInvalid} type="submit">Calculate</button>
+	</div>
+</form>
+
+<Answer {result} />
--- src/routes/linear-algebra/cramers-rule/+page.svelte
+++ src/routes/linear-algebra/cramers-rule/+page.svelte
@@ -1,22 +1,7 @@
 <script>
-	import MatrixInput from '$lib/components/MatrixInput.svelte';
-	import Answer from '$lib/components/Answer.svelte';
-
-	import { matrixIsInvalid, linearAlgebra } from '$lib/utils/misc.js';
+	import LinearAlgebra from "../LinearAlgebra.svelte";
 	import { cramersRule } from '$lib/utils/linear-algebra.js';
-
-	let A, B, size;
-	let result = null;
-
-	$: AisInvalid = matrixIsInvalid(A, size, size);
-	$: BisInvalid = matrixIsInvalid(B, size, 1);
 </script>
 
-<form on:submit|preventDefault={() => (result = linearAlgebra(cramersRule, A, B))}>
-	<MatrixInput bind:size bind:A bind:B />
-	<div class="button-zone">
-		<button disabled={AisInvalid || BisInvalid} type="submit">Calculate</button>
-	</div>
-</form>
-
-<Answer {result} />
+<h1>Cramer's Rule</h1>
+<LinearAlgebra func={cramersRule} />
