c9985221cdf885395d0dd59d23b4380069567089
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-09-13 23:58:44 +0700

    Add Gauss-Jordan page

---
+++ src/routes/linear-algebra/gauss-jordan/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import MatrixInput from '$lib/components/MatrixInput.svelte';
+	import Answer from '$lib/components/Answer.svelte';
+
+	import { joinedMatrix, matrixIsInvalid, linearAlgebra } from '$lib/utils/misc.js';
+	import { gaussJordan } from '$lib/utils/linear-algebra.js';
+
+	let A, B, size;
+	let result = null;
+
+	$: AisInvalid = matrixIsInvalid(A, size, size);
+	$: BisInvalid = matrixIsInvalid(B, size, 1);
+</script>
+
+<form on:submit|preventDefault={() => (result = linearAlgebra(gaussJordan, joinedMatrix(A, B)))}>
+	<MatrixInput bind:size bind:A bind:B />
+	<div class="button-zone">
+		<button disabled={AisInvalid || BisInvalid} type="submit">Calculate</button>
+	</div>
+</form>
+
+<Answer {result} />
