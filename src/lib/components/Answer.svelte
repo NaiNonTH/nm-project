476912ee9a95465467206615fb4e3dbe8b47c241
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-09-16 19:22:17 +0700

    Apply new CSS, precision now works with output, layout changes

--- src/lib/components/Answer.svelte
+++ src/lib/components/Answer.svelte
@@ -7,23 +7,9 @@
 	export let result = null;
 </script>
 
-{#if result != null}
-	<div id="output">
-		<h2>Output</h2>
-		<p>
-			{#if result.type === 'error'}
-				{result.message}
-			{:else if result.topic === 'root-of-equation'}
-				{result.value}
-			{:else if result.topic === 'linear-algebra'}
-				{#each result.values as value, i}
-					x<sub>{i + 1}</sub> = {value}{i == result.values.length ? '' : ', '}
-				{/each}
-			{/if}
-		</p>
-	</div>
-	{#if result.topic === 'root-of-equation' && result.progress}
-		<div class="table">
+<div id="answer">
+	{#if result != null}
+		<div>
 			<Input
 				placeholder="2"
 				label="Precision"
@@ -31,39 +17,48 @@
 				type="number"
 				bind:value={precision}
 			/>
-			<table>
-				<thead>
-					<tr>
-						{#each Object.keys(result.progress[0]) as key}
-							<th>{key}</th>
-						{/each}
-					</tr>
-				</thead>
-				<tbody>
-					{#each result.progress as row}
+		</div>
+		<div id="output">
+			<h2>Output</h2>
+			<p>
+				{#if result.type === 'error'}
+					{result.message}
+				{:else if result.topic === 'root-of-equation'}
+					{result.value.toFixed(precision)}
+				{:else if result.topic === 'linear-algebra'}
+					{#each result.values as value, i}
+						x<sub>{i + 1}</sub> = {value}{i == result.values.length ? '' : ', '}
+					{/each}
+				{/if}
+			</p>
+		</div>
+		{#if result.topic === 'root-of-equation' && result.progress}
+			<div class="table">
+				<table>
+					<thead>
 						<tr>
-							{#each Object.values(row) as data}
-								<td>{+data.toFixed(Math.trunc(precision))}</td>
+							{#each Object.keys(result.progress[0]) as key}
+								<th>{key}</th>
 							{/each}
 						</tr>
-					{/each}
-				</tbody>
-			</table>
-		</div>
+					</thead>
+					<tbody>
+						{#each result.progress as row}
+							<tr>
+								{#each Object.values(row) as data}
+									<td>{+data.toFixed(Math.trunc(precision))}</td>
+								{/each}
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
+		{/if}
 	{/if}
-{/if}
+</div>
 
 <style>
 	.table {
 		overflow: auto;
-	}
-	table {
-		min-width: fit-content;
-		width: 100%;
-		text-align: left;
-	}
-	table th,
-	table td {
-		padding: 0.2rem;
 	}
 </style>
