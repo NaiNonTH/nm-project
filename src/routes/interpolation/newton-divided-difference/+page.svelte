a95748c13bc221f2ec5b7ce2945218f070ffd125
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-09-16 19:42:07 +0700

    Remove unused code and use component instead

--- src/routes/interpolation/newton-divided-difference/+page.svelte
+++ src/routes/interpolation/newton-divided-difference/+page.svelte
@@ -1,74 +1,7 @@
 <script>
-	import Input from '$lib/components/Input.svelte';
+	import Interpolation from '../Interpolation.svelte';
 
 	import { newtonDividedDifference } from '$lib/utils/interpolation.js';
-
-	const subNumber = ['₀', '₁', '₂', '₃', '₄', '₅', '₆', '₇', '₈', '₉'];
-
-	$: count = count < 2 ? 2 : count;
-
-	let x;
-
-	let x_data = [];
-	let y_data = [];
-	let filter = new Set();
-
-	$: x_filtered = x_data.filter((_, i) => i < count && filter.has(i));
-	$: y_filtered = y_data.filter((_, i) => i < count && filter.has(i));
-
-	$: disabled =
-		x_data.some((num) => typeof num === 'number') ||
-		y_data.some((num) => typeof num === 'number') ||
-		filter.size < 2;
-
-	function toggleFilter(checked, i) {
-		if (!checked) filter.delete(i);
-		else filter.add(i);
-
-		filter = filter;
-	}
-
-	let result;
-
-	function submit() {
-		result = newtonDividedDifference(x, x_filtered, y_filtered);
-	}
 </script>
 
-<form on:submit|preventDefault={submit}>
-	<div class="same-line">
-		<Input label="Data Count" name="count" type="number" placeholder="3" bind:value={count} />
-		<Input label="Missing x" name="x" type="number" placeholder="40331" bind:value={x} />
-	</div>
-	<div>
-		{#each Array(count) as _, i}
-			<div class="same-line">
-				<input
-					on:change={(event) => toggleFilter(event.target.checked, i)}
-					type="checkbox"
-					name="pick-x{i + 1}"
-					id="pick-x{i + 1}"
-				/>
-				<Input
-					label="x of Data No.{i + 1}"
-					placeholder="x{subNumber[i]}"
-					name="x{i}"
-					type="number"
-					bind:value={x_data[i]}
-				/>
-				<Input
-					label="f(x) of Data No.{i + 1}"
-					placeholder="f(x{subNumber[i]})"
-					name="f(x{i})"
-					type="number"
-					bind:value={y_data[i]}
-				/>
-			</div>
-		{/each}
-	</div>
-	<div class="button-zone">
-		<button {disabled} type="submit">Calculate</button>
-	</div>
-</form>
-
-<p>{result}</p>
+<Interpolation func={newtonDividedDifference} />
