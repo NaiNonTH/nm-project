ea163301366cba90be5b054752ae707744db25e7
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-09-27 23:29:59 +0700

    New Landing page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,30 @@
-<h1>
-	<span aria-hidden="true">🔥</span> Welcome to <s>HELL</s> <span aria-hidden="true">🔥</span>
-</h1>
-<p>
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, fuga aperiam odit eos, blanditiis
-	id, cumque quo dicta quam quod optio. Voluptas illum maxime assumenda laboriosam quibusdam quo,
-	similique commodi.
-</p>
+<section class="hero">
+	<img width="360" height="280" src="dashboard.svg" alt="Dashboard by unDraw">
+	<div>
+		<h1>Math, Data, Empowered.</h1>
+		<p>Please select one of the topics from the sidebar on your left.</p>
+	</div>
+</section>
+
+<style>
+	.hero {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 2rem;
+	}
+	.hero * {
+		width: 100%;
+		text-align: center;
+	}
+	.hero div * {
+		margin: 0.5rem 0;
+	}
+	.hero h1 {
+		font-size: 2rem;
+	}
+	.hero p {
+		opacity: 0.75;
+	}
+</style>
